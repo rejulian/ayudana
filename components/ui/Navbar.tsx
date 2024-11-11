c1c626dc7fd1ee087ff97f5f204fd9b28216ff10
rejulian <contacto.julian.re@gmail.com>
2024-11-10 21:57:33 -0300

    modificando boton agregar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -2,7 +2,7 @@ import { createClient } from "@/utils/supabase/server"
 import Link from "next/link"
 import { Button } from "./button"
 import { signout } from "@/app/actions/auth"
-import { House } from "lucide-react";
+import { House, Plus } from "lucide-react";
 
 export default async function NavBar() {
     const supabase = await createClient()
@@ -12,7 +12,7 @@ export default async function NavBar() {
     return <nav className="flex gap-3 p-4 justify-end">
         {user ? <>
             <Link href="/" className="flex items-center gap-2 border px-3 py-2 max-w-[100px] rounded-lg hover:bg-gray-900"><House className="size-4" />Inicio</Link>
-            <Link className="border p-2 rounded-md hover:bg-gray-900" href="/agregar">Agregar alojamiento</Link>
+            <Link className="flex items-center gap-2 border p-2 rounded-md hover:bg-gray-900" href="/agregar"><Plus className="size-4" /> Agregar</Link>
             <form action={signout}>
                 <Button type="submit">Cerrar sesion</Button>
             </form>
